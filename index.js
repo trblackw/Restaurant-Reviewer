e7c0110de977088d36e3cab0e256f02f910834b5
Tucker <trblackw@ncsu.edu>
2019-09-08 16:57:22 -0400

    Set up location services via Geolocation

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import { name as appName } from './app.json';
+console.disableYellowBox = true;
+import Geolocation from '@react-native-community/geolocation';
+Geolocation.setRNConfiguration({ skipPermissionRequests: false, authorizationLevel: 'always' });
 
 AppRegistry.registerComponent(appName, () => App);
