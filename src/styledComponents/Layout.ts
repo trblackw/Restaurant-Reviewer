8e045f70a677bb6f377352aed5b4863bb3e5c114
Tucker <trblackw@ncsu.edu>
2019-09-09 17:15:43 -0400

    Add to layout components

--- src/styledComponents/Layout.ts
+++ src/styledComponents/Layout.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+
+export const ViewContainer = styled(View)`
+   flex: 1
+`
 
 export const ViewHeader = styled(Text)`
    padding: 40px;
@@ -7,4 +11,12 @@ export const ViewHeader = styled(Text)`
    text-align: center;
    color: ${({ color = '#0066CC' }: { color?: string }) => color};
    font-weight: 300;
+`;
+
+export const PreviewContainer = styled(View)`
+justify-content: space-between;
+align-items: center;
+flex-direction: row;
+padding: 5px;
+background-color: ${({ even }: { even: boolean }) => (even ? 'hsl(0, 0%, 86%)' : 'transparent')}
 `;
