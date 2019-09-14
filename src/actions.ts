ef646b62f10f4b8fc75adc56d885ec903177133f
Tucker <trblackw@ncsu.edu>
2019-09-14 12:48:46 -0400

    Add actions (and thunk types) for fetching establishments and cuisines

--- src/actions.ts
+++ src/actions.ts
@@ -1,12 +1,28 @@
-import { Coordinates, Establishment } from './types';
 import * as constants from './constants';
+import * as types from './types';
 import { Dispatch } from 'redux';
 import http, { EndPoints } from './http';
+/**
+ * @ThunkWithArgs
+ * <typdefs for params, typedefs for payload>
+ * @ThunkNoArgs
+ * <typedefs for payload>
+ * --------------------------------------------
+ * 2 separate types are used to avoid unnecessary null checks for actionCreator args
+ */
 
-export const fetchEstablishments = async (userLocation: Coordinates) => {
+export const fetchEstablishments: types.ThunkWithArgs<types.Coordinates, types.Establishment[]> = userLocation => async (
+   dispatch: Dispatch
+) => {
    const { latitude, longitude } = userLocation;
-   const establishments = await http<{ establishment: Establishment }[]>(EndPoints.establishments, { latitude, longitude });
+   const establishments = await http<{ establishment: types.Establishment }[]>(EndPoints.establishments, { latitude, longitude });
 
-   return (dispatch: Dispatch) =>
-      dispatch({ type: constants.FETCH_ESTABLISHMENTS, payload: establishments.map(({ establishment }) => establishment) });
+   return dispatch({ type: constants.FETCH_ESTABLISHMENTS, payload: establishments.map(({ establishment }) => establishment) });
+};
+
+export const fetchCuisines: types.ThunkWithArgs<types.Coordinates, types.Cuisine[]> = userLocation => async (dispatch: Dispatch) => {
+   const { latitude, longitude } = userLocation;
+   const cuisines = await http<{ cuisine: types.Cuisine }[]>(EndPoints.cuisines, { latitude, longitude });
+
+   return dispatch({ type: constants.FETCH_CUISINES, payload: cuisines.map(({ cuisine }) => cuisine) });
 };
