0f8495c4e7cf7794d823665bf0e467690018f688
Tucker <trblackw@ncsu.edu>
2019-09-28 18:49:52 -0400

    Handle param logic

--- src/http.ts
+++ src/http.ts
@@ -16,18 +16,24 @@ export enum EndPoints {
 
 const locationBasedEndpoints = [EndPoints.collections, EndPoints.cuisines, EndPoints.establishments, EndPoints.cities, EndPoints.search];
 
-export default async <T>(endpoint: EndPoints, params?: Partial<Coordinates>): Promise<T> => {
+export default async <T>(endpoint: EndPoints, params?: any): Promise<T> => {
+console.log("TCL: params", params)
    let _url: string = `https://developers.zomato.com/api/v2.1/${endpoint}?`;
    // append user coordinates if location-based endpoint
    if (locationBasedEndpoints.indexOf(endpoint) > -1 && params) {
       //Todo: handle params other than lat and lng in url
       _url = _url.concat(`lat=${params.latitude}&lon=${params.longitude}`);
    }
+   if (Object.keys(params).length > 2) {
+      //@ts-ignore
+      _url = _url.concat(`&${Object.keys(params)[2]}=${Object.values(params)[2][Object.keys(params)[2]]}`)
+   }
+   console.log('_url', _url)
    return new Promise(async resolve => {
       const response = await fetch(_url, {
          headers: { 'Content-Type': 'application/json', 'X-Zomato-API-Key': 'b80a51fb71c77dec30df59dff753eda5' }, method: 'GET'
       });
       const json = await response.json();
-      resolve(json[endpoint]);
+      resolve(json[endpoint] ? json[endpoint] : json);
    })
 };
