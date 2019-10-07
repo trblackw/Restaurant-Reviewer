bcc6b16bfb7d33f47e5fe0dcfbc4b6cf15706254
Tucker <trblackw@ncsu.edu>
2019-10-06 20:26:00 -0400

    Add linking info button  to establishment preview

--- src/components/EstablishmentPreview.tsx
+++ src/components/EstablishmentPreview.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import { Establishment } from '../types';
-import styled from 'styled-components'
-import { Text } from 'react-native';
+import { Establishment, Views } from '../types';
+import styled from 'styled-components';
+import { Text, View } from 'react-native';
 import { PreviewContainer } from '../styledComponents/Layout';
+import InfoButton from './InfoButton';
+import useNavigation from '../hooks/useNavigation';
 
-interface Props extends Partial<Establishment> {
+interface Props extends Establishment {
    index: number;
 }
-const EstablishmentPreview: React.FC<Props> = ({ name, index }) => {
+const EstablishmentPreview: React.FC<Props> = ({ name, index, id }) => {
+   const navigation = useNavigation();
    return (
       <PreviewContainer even={index % 2 === 0}>
-         <Name>{name}</Name>
+         <View>
+            <Name>{name}</Name>
+         </View>
+         <InfoButton label="Establishment Info" onPress={() => navigation.navigate(Views.EstablishmentInfo, { establishmentId: id })}>Info</InfoButton>
       </PreviewContainer>
    );
-}
+};
 
 export default EstablishmentPreview;
 
